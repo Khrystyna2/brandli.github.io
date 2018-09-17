efe5bdf95f7e2bc8cae19032ace08c798c46aafb
Khrystyns Khozhai <l.xrustja@gmail.com>
2018-09-17 14:35:52 +0300

    added new img

---
+++ js/main-script.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+    $('#eng, #ru').click(function() {
+		$('#ru, #eng').toggleClass('change-language__active');
+	});
+
+
+    //style input
+    var $inputs = $(".input-form input, .input-form textarea");
+
+    $inputs.on('focus', function() {
+      var $self =  $(this);
+      $self.prev().addClass('active');
+    });
+
+    $inputs.on('focusout', function() {
+      var $self =  $(this);
+      $self.val() == "" ? $self.prev().removeClass('active') : "";
+    });
+
+
+
+});
